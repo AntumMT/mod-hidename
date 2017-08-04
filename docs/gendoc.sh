ef22c0cc4ff2a316e2a327e3e06986fdb2ce6b56
AntumDeluge <antumdeluge@gmail.com>
2017-08-04 16:16:20 -0700

    Change API documentation URL

--- docs/gendoc.sh
+++ docs/gendoc.sh
@@ -4,7 +4,7 @@ DOCS="$(dirname $(readlink -f $0))"
 ROOT="$(dirname ${DOCS})"
 
 CONFIG="${DOCS}/config.ld"
-OUT="${DOCS}"
 
 cd "${ROOT}"
-ldoc -B -c "${CONFIG}" -d "${OUT}" ./
+rm -rf "${DOCS}/api.html" "${DOCS}/modules" "${DOCS}/scripts"
+ldoc -B -c "${CONFIG}" -o "api" -d "${DOCS}" ./
